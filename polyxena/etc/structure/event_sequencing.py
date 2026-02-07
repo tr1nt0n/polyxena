19370ea4e6ed0370d723a62902667c7b68bb5936
trintonprater <trinton.k.prater@gmail.com>
2026-02-07 16:00:16 -0500

    moment sequencing

--- polyxena/etc/structure/event_sequencing.py
+++ polyxena/etc/structure/event_sequencing.py
@@ -8,4 +8,47 @@ import math
 import random
 from sympy import combinatorics
 from polyxena import library
-from polyxena import ts
+from polyxena import rhythm
+
+# moments
+
+moment_amount_sequence = [3, 4, 5, 6, 7, 3, 5, 6, 7, 4, 3, 6, 7, 4, 5, 3, 4, 7, 6, 5]
+
+retrograded_moment_amount_sequence = moment_amount_sequence[::-1]
+
+transition_types = ["microformal", "oppositional", "incised", "evolutional"]
+
+transition_type_sequence = []
+
+for _ in library.tetrahedron_corners_list:
+    transition_type_sequence.append(transition_types[_])
+
+retrograded_transition_type_sequence = transition_type_sequence[::-1]
+
+for _ in range(0, 9):
+    section_number = _ + 1
+    gambe_moment_amount = moment_amount_sequence[_]
+    theorbe_moment_amount = retrograded_moment_amount_sequence[_]
+
+    print(f"In section {section_number}, the Gambe has {gambe_moment_amount} moments")
+
+    for _ in range(0, gambe_moment_amount - 1):
+        transition_number = _ + 1
+        print(
+            f"Transition {transition_number} between moments {1 + _} and {2 + _} is {transition_type_sequence[_]}"
+        )
+
+    print("")
+
+    print(
+        f"In section {section_number}, the Theorbe has {theorbe_moment_amount} moments"
+    )
+
+    for _ in range(0, theorbe_moment_amount - 1):
+        transition_number = _ + 1
+        print(
+            f"Transition {transition_number} between moments {1 + _} and {2 + _} is {retrograded_transition_type_sequence[_]}"
+        )
+
+    print("_______________")
+    print("")
--- polyxena/rhythm.py
+++ polyxena/rhythm.py
@@ -4,6 +4,70 @@ import baca
 import evans
 import trinton
 import itertools
+import math
 import random
+from sympy import combinatorics
 from polyxena import library
-from polyxena import pitch
+
+
+# rhythm development
+
+prolation_corners = trinton.rotated_sequence(library.tetrahedron_corners_list, 0)
+denominator_corners = trinton.rotated_sequence(
+    library.retrograded_tetrahedron_corners_list, 1
+)
+elaboration_corners = trinton.rotated_sequence(library.tetrahedron_corners_list, 2)
+tremolo_corners = trinton.rotated_sequence(
+    library.retrograded_tetrahedron_corners_list, 3
+)
+accent_density_corners = trinton.rotated_sequence(library.tetrahedron_corners_list, 4)
+
+prolations = [(1, 2), (1, 1), (3, 2), (7, 4)]
+denominators = [4, 8, 16, 32]
+elaborations = [
+    "no elaborations",
+    "an elaboration on the smallest durations",
+    "an elaboration on the smallest and second smallest durations",
+    "an elaboration on all but the highest durations",
+]
+tremoli = [
+    "no tremoli",
+    "tremoli on the smallest durations",
+    "tremoli on the smallest and second smallest durations",
+    "tremoli on every tie",
+]
+accent_densities = [
+    "no accents",
+    "accents on the smallest durations",
+    "accents on the smallest and second smallest durations",
+    "accents on all but the longest duration",
+]
+
+prolation_sequence = []
+denominator_sequence = []
+elaboration_sequence = []
+tremolo_sequence = []
+accent_density_sequence = []
+
+for (
+    prolation_index,
+    denominator_index,
+    elaboration_index,
+    tremolo_index,
+    accent_density_index,
+) in zip(
+    prolation_corners,
+    denominator_corners,
+    elaboration_corners,
+    tremolo_corners,
+    accent_density_corners,
+):
+    prolation_sequence.append(prolations[prolation_index])
+    denominator_sequence.append(denominators[denominator_index])
+    elaboration_sequence.append(elaborations[elaboration_index])
+    tremolo_sequence.append(tremoli[tremolo_index])
+    accent_density_sequence.append(accent_densities[accent_density_index])
+
+# talea sequence
+
+root = [4, 6, 7, 3, 1, 1, 2]
